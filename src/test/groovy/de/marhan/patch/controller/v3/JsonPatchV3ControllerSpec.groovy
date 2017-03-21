fb4f0b3874e7cdf1b8ad42f74bfb64a2bc353bcf
Markus Hanses <me@markushanses.de>
2017-03-21 22:04:44 +0100

    Json patch replace list added.

--- src/test/groovy/de/marhan/patch/controller/v3/JsonPatchV3ControllerSpec.groovy
+++ src/test/groovy/de/marhan/patch/controller/v3/JsonPatchV3ControllerSpec.groovy
@@ -69,5 +69,25 @@ class JsonPatchV3ControllerSpec extends SpringBootSpecification {
 
     }
 
+    def "patch replace address"() {
+
+        given:
+
+        def body = '[{ "op": "replace", "path": "/addresses", "value": [{"city": "Hannover", "street": "Hauptstrasse 100"}, {"city": "Bremen", "street": "Bahnhofstrasse 99"}] }]'
+
+        expect:
+
+        given().contentType(PATCH_CONTENT_TYPE)
+                .body(body)
+                .when().patch("/v3/persons/1")
+                .then().statusCode(200)
+                .body("id", equalTo(1))
+                .body("name", equalTo("Fritz Brause"))
+                .body("addresses.findAll", hasSize(2))
+                .body("addresses.findAll { it.city == 'Bremen'}.street", hasItem("Bahnhofstrasse 99"))
+                .body("addresses.findAll { it.city == 'Hannover'}.street", hasItem("Hauptstrasse 100"))
+
+    }
+
 
 }
