ac59e5f281d2463ec129195cfba7a8b4c50b2895
Markus Hanses <me@markushanses.de>
2017-03-21 21:33:59 +0100

    Json patch v3 controller improved and tests added.

--- src/main/java/de/marhan/patch/controller/v3/JsonPatchV3Controller.java
+++ src/main/java/de/marhan/patch/controller/v3/JsonPatchV3Controller.java
@@ -3,9 +3,10 @@ package de.marhan.patch.controller.v3;
 import de.marhan.patch.controller.common.JsonPatcher;
 import de.marhan.patch.controller.common.RestMediaType;
 import de.marhan.patch.controller.resource.PersonResource;
-import de.marhan.patch.service.PersonService;
+import de.marhan.patch.controller.resource.ResourceBuilder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -14,27 +15,23 @@ import java.util.Optional;
 @RestController
 public class JsonPatchV3Controller {
 
-    private PersonService service;
 
     private JsonPatcher jsonPatcher;
 
     @Autowired
-    public JsonPatchV3Controller(PersonService service, JsonPatcher jsonPatcher) {
-        this.service = service;
+    public JsonPatchV3Controller(JsonPatcher jsonPatcher) {
         this.jsonPatcher = jsonPatcher;
     }
 
-    @RequestMapping(
-            value = "/v3/persons/{id}",
-            method = RequestMethod.PATCH,
-            consumes = RestMediaType.APPLICATION_PATCH_JSON_VALUE)
-    @ResponseStatus(HttpStatus.NO_CONTENT)
-    public ResponseEntity<Void> updatePartial(@PathVariable Integer id, @RequestBody String resource) {
-        PersonResource target = new PersonResource();
-        target.setId(id);
+    @RequestMapping(value = "/v3/persons/{id}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<PersonResource> get(@PathVariable Long id) {
+        return new ResponseEntity<>(new ResourceBuilder().build(), HttpStatus.OK);
+    }
 
-        Optional<PersonResource> patched = jsonPatcher.patch(resource, target);
-
-        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    @RequestMapping(value = "/v3/persons/{id}", method = RequestMethod.PATCH, consumes = RestMediaType.APPLICATION_PATCH_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<PersonResource> updatePartial(@PathVariable Integer id, @RequestBody String updateResource) {
+        PersonResource resource = new ResourceBuilder().build();
+        Optional<PersonResource> patched = jsonPatcher.patch(updateResource, resource);
+        return new ResponseEntity<>(patched.get(), HttpStatus.OK);
     }
 }
--- src/test/groovy/de/marhan/patch/controller/v3/JsonPatchV3ControllerSpec.groovy
+++ src/test/groovy/de/marhan/patch/controller/v3/JsonPatchV3ControllerSpec.groovy
@@ -1,23 +1,51 @@
 package de.marhan.patch.controller.v3
 
 import de.marhan.patch.controller.SpringBootSpecification
+import io.restassured.http.ContentType
+
+import static io.restassured.RestAssured.given
+import static org.hamcrest.CoreMatchers.equalTo
+import static org.hamcrest.CoreMatchers.hasItem
+import static org.hamcrest.collection.IsCollectionWithSize.hasSize
 
 class JsonPatchV3ControllerSpec extends SpringBootSpecification {
 
-    public static final String CONTENT_TYPE_VALUE = "application/json-patch+json"
 
-    def "patch replace"() {
+    static final String PATH = "/v3/persons/1"
+    static final String PATCH_CONTENT_TYPE = "application/json-patch+json"
 
-        given:
-
-        def body = '[{ "op": "replace", "path": "/name", "value": "my new name" }]'
+    def "get person"() {
 
         expect:
 
-        io.restassured.RestAssured.given().contentType(CONTENT_TYPE_VALUE)
+        given().contentType(ContentType.URLENC)
+                .when().get(PATH)
+                .then().contentType(ContentType.JSON)
+                .statusCode(200)
+                .body("id", equalTo(1))
+                .body("name", equalTo("Fritz Brause"))
+                .body("addresses.findAll", hasSize(2))
+                .body("addresses.findAll { it.city == 'Hamburg'}.street", hasItem("Spitalerstrasse 12"))
+                .body("addresses.findAll { it.city == 'Bremen'}.street", hasItem("Boetcherstrasse 2"))
+    }
+
+    def "patch replace name"() {
+
+        given:
+
+        def body = '[{ "op": "replace", "path": "/name", "value": "Jona Meier" }]'
+
+        expect:
+
+        given().contentType(PATCH_CONTENT_TYPE)
                 .body(body)
                 .when().patch("/v3/persons/1")
-                .then().statusCode(204)
+                .then().statusCode(200)
+                .body("id", equalTo(1))
+                .body("name", equalTo("Jona Meier"))
+                .body("addresses.findAll", hasSize(2))
+                .body("addresses.findAll { it.city == 'Hamburg'}.street", hasItem("Spitalerstrasse 12"))
+                .body("addresses.findAll { it.city == 'Bremen'}.street", hasItem("Boetcherstrasse 2"))
 
     }
 
